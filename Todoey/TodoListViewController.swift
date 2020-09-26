4505be8b52608fd2b26e7f6a27dd5c3f418d960d
Sean Barton <linglither@gmail.com>
2020-09-26 15:02:03 -0600

    dev: setup tableview and test with dummy data
    Set the tableView number of rows and cells to display the dummy data

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get the string in the array at the current row
+        let item = itemArray[indexPath.row]
+        
+        // Get the cell at the current row
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        // Set the cell text to the item text
+        cell.textLabel?.text = item
+        
+        // Return the cell
+        return cell
+    }
 }
-
